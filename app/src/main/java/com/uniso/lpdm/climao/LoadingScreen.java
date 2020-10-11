a82da159acef85612e07efaad671a745351c745e
Erick Batista <erick.batista@helpper.com.br>
2020-10-11 18:40:40 -0300

    Create LoadingScreen, in progress

--- app/src/main/java/com/uniso/lpdm/climao/LoadingScreen.java
+++ app/src/main/java/com/uniso/lpdm/climao/LoadingScreen.java
@@ -3,15 +3,12 @@ package com.uniso.lpdm.climao;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+public class LoadingScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Intent navigateTo = new Intent(this, LoadingScreen.class);
-        startActivity(navigateTo);
+        setContentView(R.layout.activity_loading_screen);
     }
 }
