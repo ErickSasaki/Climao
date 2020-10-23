2fcac66f5488e66cdd578905a988e93453e37b11
Erick Batista <erick.batista@helpper.com.br>
2020-10-22 21:09:45 -0300

    NetworkUtils

---
+++ app/src/main/java/com/uniso/lpdm/climao/EndpointsInterface.java
@@ -0,0 +1,5 @@
+package com.uniso.lpdm.climao;
+
+public interface EndpointsInterface {
+    // @GET("data/2.5")
+}
--- app/src/main/java/com/uniso/lpdm/climao/Home.java
+++ app/src/main/java/com/uniso/lpdm/climao/Home.java
@@ -3,6 +3,18 @@ package com.uniso.lpdm.climao;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import retrofit2.Retrofit;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +22,15 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        final TextView textView = (TextView) findViewById(R.id.test);
+        textView.setText("new text!");
+
+        String cep = "18066007";
+        final String url = "http://viacep.com.br/ws/" + cep + "/json/";
+
+        Retrofit retrofit = NetworkUtils.getRetrofitInstance(url);
+
+
     }
 }
