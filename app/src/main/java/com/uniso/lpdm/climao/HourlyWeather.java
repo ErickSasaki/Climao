642b82be470671f99190590212ee14951e9084a7
Erick Batista <erick.batista@helpper.com.br>
2020-11-28 18:37:32 -0300

    create hourly weather screen

---
+++ app/src/main/java/com/uniso/lpdm/climao/HourlyWeather.java
@@ -0,0 +1,13 @@
+package com.uniso.lpdm.climao;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HourlyWeather extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hourly_weather);
+    }
+}
